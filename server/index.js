9c385ddf7fc3413d613a6b5ca970bad14ccf83a6
Sudarshan <155318043+sudarshan1212@users.noreply.github.com>
2024-08-10 15:51:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use("/product", productRoutes);
 app.use("/transaction", transactionRoutes);
 
 const aquaBlue = chalk.hex("#00FFFF");
-
+//PORT
 app.listen(port, () =>
   console.log(
     aquaBlue(`THE SERVER IS RUNNING ON: ${chalk.hex("#FFC0CB").bold(port)} !`)
